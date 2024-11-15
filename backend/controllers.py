e97d01741088aefc0d873648e2bf88be5e6f2035
Krishna Goel <23f3003674@ds.study.iitm.ac.in>
2024-11-15 23:08:06 +0530

    commit on 15/11

--- backend/controllers.py
+++ backend/controllers.py
@@ -15,12 +15,16 @@ def signin():
         if usr and usr.role ==0:
             return render_template("admin_dashboard.html")
 
-        if usr and usr.role ==1 and usr.status=="approve":
+        elif usr and usr.role ==1 and usr.status=="approve":
             return render_template("customer_dashboard.html")
         
-        if usr and usr.role ==2 and usr.status=="approve":
+        elif usr and usr.role ==2 and usr.status=="approve":
             return render_template("professional_dashboard.html")
-    return render_template("login.html")
+        
+        else:
+            return render_template("login.html",msg ="INVALID USER CREDENTIALS")
+
+    return render_template("login.html",msg="")
 
 @app.route("/signup_customer",methods=["GET","POST"])
 def customer_signup():
@@ -30,6 +34,9 @@ def customer_signup():
         full_name=request.form.get("full_name")
         address=request.form.get("address")
         pincode= request.form.get("pincode")
+        usr= User_Login.query.filter_by(email=uname).first()
+        if usr:
+            return render_template("login.html",msg="THIS MAIL IS ALREADY REGISTERED")
 
         new_usr1= Customer(full_name=full_name,address=address,pincode=pincode)
         db.session.add(new_usr1)
@@ -38,7 +45,7 @@ def customer_signup():
         new_usr = User_Login(email=uname,password=pwd,role=1,customer_id=new_usr1_id,status="approve")
         db.session.add(new_usr)
         db.session.commit()
-        return render_template("login.html")
+        return render_template("login.html",msg="REGISTERED SUCCESSFULLY!")
 
     return render_template("signup_customer.html")
 
@@ -50,6 +57,10 @@ def professional_signup():
         full_name=request.form.get("full_name")
         address=request.form.get("address")
         pincode= request.form.get("pincode")
+        usr= User_Login.query.filter_by(email=uname).first()
+        if usr:
+            return render_template("login.html",msg="THIS MAIL IS ALREADY REGISTERED")
+
         experience= request.form.get("experience")
         service_name = request.form.get("service")
                   
