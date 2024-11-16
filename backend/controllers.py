dead04db5042885d2884ee1dbc3f03451c87cedd
Krishna Goel <23f3003674@ds.study.iitm.ac.in>
2024-11-16 17:36:18 +0530

    commit on 16/11/24

--- backend/controllers.py
+++ backend/controllers.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request,url_for,redirect
 from .models import *
 from flask import current_app as app
 
@@ -13,13 +13,14 @@ def signin():
         pwd = request.form.get("password")
         usr= User_Login.query.filter_by(email=uname,password=pwd).first()
         if usr and usr.role ==0:
-            return render_template("admin_dashboard.html")
+            
+            return redirect(url_for("admin_dashboard",name=uname))
 
         elif usr and usr.role ==1 and usr.status=="approve":
-            return render_template("customer_dashboard.html")
+            return redirect(url_for("customer_dashboard",name=uname))
         
         elif usr and usr.role ==2 and usr.status=="approve":
-            return render_template("professional_dashboard.html")
+            return redirect(url_for("professional_dashboard",name=uname))
         
         else:
             return render_template("login.html",msg ="INVALID USER CREDENTIALS")
@@ -76,3 +77,59 @@ def professional_signup():
         return render_template("login.html")
 
     return render_template("signup_professional.html")
+
+
+# common route for admin
+@app.route("/admin/<name>")
+def admin_dashboard(name):
+    services = get_services()
+    return render_template("admin_dashboard.html",name=name,services=services)
+
+@app.route("/customer/<name>")
+def customer_dashboard(name):
+    services = get_services()
+    return render_template("customer_dashboard.html",name=name,services=services)
+
+@app.route("/professional/<name>")
+def professional_dashboard(name):
+    return render_template("professional_dashboard.html",name=name)
+
+
+
+@app.route("/service/<name>",methods=["POST","GET"])
+def add_service(name):
+    if request.method =="POST":
+        sname=request.form.get("name")
+        sdescription=request.form.get("description")
+        sbase_price=request.form.get("base_price")
+        stime_required=request.form.get("time_required")
+        new_service = Service(name=sname,description=sdescription,base_price=sbase_price,time_required=stime_required)
+        db.session.add(new_service)
+        db.session.commit()
+
+        return redirect(url_for("admin_dashboard",name=name))
+
+    return render_template("add_service.html",name=name)
+
+
+def get_services():
+    services = Service.query.all()
+    return services
+
+#def get_services_request():
+ #   Service_requests = Service_request.query.all()
+  #  return service_requests
+
+#@app.route("/service_rating/<name>",methods=["POST","GET"])
+#def add_service(name):
+ #   if request.method =="POST":
+  #      srating=request.form.get("rating")
+  #      sremark=request.form.get("remark")
+        
+  #      new_service_request = Service_request(rating=srating,remark=sremark)
+  #      db.session.add(new_service_request)
+  #      db.session.commit()
+
+   #     return redirect(url_for("customer_dashboard",name=name))
+
+   # return render_template("service_rating.html",name=name)
