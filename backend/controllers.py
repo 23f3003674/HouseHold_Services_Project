433f429558fcde95ec1103639a227e24bcb7347b
Krishna Goel <23f3003674@ds.study.iitm.ac.in>
2024-11-19 14:08:56 +0530

    commit on 19/11/24 at 2

--- backend/controllers.py
+++ backend/controllers.py
@@ -98,7 +98,9 @@ def customer_dashboard(id):
     # u_id = u.customer_id
     customer = get_customer(id)
     services = get_services()
-    return render_template("customer_dashboard.html",services=services,customer=customer,id=id)
+    services1 = get_services()
+    service_requests = Service_request().query.filter_by(customer_id=id).all()
+    return render_template("customer_dashboard.html",services=services,customer=customer,id=id,service_requests=service_requests,services1=services1)
 
 @app.route("/professional/<name>")
 def professional_dashboard(name):
@@ -157,9 +159,13 @@ def approve_professional(name):
 
 @app.route("/service_request/<id>", methods=["GET","POST"])
 def add_service_request(id):
+    services1 = get_services()
+    services = get_services()
+    service_requests = Service_request().query.filter_by(customer_id=id).all()
     if request.method=="POST":
         c_id = request.form.get("id")
         s_id = request.form.get("s_id")
+
 
         new_service_request = Service_request(customer_id=c_id,service_id=s_id)
         db.session.add(new_service_request)
@@ -177,22 +183,26 @@ def search_ad(name):
         by_customers = search_by_customers(search_txt)
         by_professionals = search_by_professionals(search_txt)
         by_services = search_by_services(search_txt)
+        customers = Customer.query.all()
         if by_services:
-            return render_template("admin_dashboard.html",name =name,services=by_services,professionals=professionals,user_logins=user_logins)
+            return render_template("admin_dashboard.html",name =name,services=by_services,professionals=professionals,user_logins=user_logins,customers=customers)
         elif by_professionals:
-            return render_template("admin_dashboard.html",name =name,services=services,professionals1=by_professionals,professionals=professionals,user_logins=user_logins)
+            return render_template("admin_dashboard.html",name =name,services=services,professionals1=by_professionals,professionals=professionals,user_logins=user_logins,customers=customers)
         elif by_customers:
-            return render_template("admin_dashboard.html",name =name,services=services,customers1=by_customers,professionals=professionals,user_logins=user_logins)
+            return render_template("admin_dashboard.html",name =name,services=services,customers1=by_customers,professionals=professionals,user_logins=user_logins,customers=customers)
 
     return redirect(url_for("admin_dashboard",name=name))
 
 @app.route("/search_c/<id>",methods =["GET","POST"])
 def search_c(id):
+    # customer = get_customer(id)
+    services1 = get_services()
+    service_requests = Service_request().query.filter_by(customer_id=id).all()
     if request.method=="POST":
         search_txt = request.form.get("search_txt")
         by_services = search_by_services(search_txt)
         if by_services:
-            return render_template("customer_dashboard.html",id =id,services=by_services)
+            return render_template("customer_dashboard.html",id =id,services=by_services,service_requests=service_requests,services1=services1)
 
     return redirect(url_for("customer_dashboard",id=id))
 
